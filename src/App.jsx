25ee212a4feece8d0dfebda923de50c3e6885994
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-09-28 00:26:39 +0000

    basic layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen w-screen justify-center items-center gap-4">
+      <div className="min-h-screen w-1/2 flex justify-end items-center">
+        left
+
+      </div>
+      <div className="min-h-screen w-1/2 flex justify-start items-center">
+        right
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-sky-700 text-white font-bold flex justify-center items-center">
+      <h1 className="text-3xl p-4">
+        CV Generator
+      </h1>
+    </div>
+  )
+}
