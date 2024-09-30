a70a09722aa23b9e5dc3cd33b8d7a44d9d9e5d13
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-09-30 23:49:46 +0000

    props for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import CV from "./components/CV";
+
 export default function App() {
   return (
     <div className="flex min-h-screen w-screen justify-center items-center gap-4">
       <div className="min-h-screen w-1/2 flex justify-end items-center">
         left
-
       </div>
-      <div className="min-h-screen w-1/2 flex justify-start items-center">
-        right
+      <div className="min-h-screen w-1/2 flex justify-center items-center bg-red-300">
+        <CV name='Mateusz' surname='Karpiński' email='mateusz.karpinski29@gmail.com' birth='15.10.2003' country='Poland' />
       </div>
     </div>
-  )
+  );
 }
+
---
+++ src/components/CV.jsx
@@ -0,0 +1,12 @@
+export default function CV(props) {
+  return (
+    <div className="outline-black outline-1 bg-blue-300 w-[90%] h-full">
+      <h1 className="font-bold text-3xl">{props.name} {props.surname}</h1>
+      <h2><span className="font-semibold"> Email:</span> {props.email}</h2>
+      <h2><span className="font-semibold"> Phone:</span> {props.phone}</h2>
+      <h2><span className="font-semibold"> Birth date:</span> {props.birth}</h2>
+      <h2><span className="font-semibold"> Country:</span> {props.country}</h2>
+    </div>
+  );
+}
+
